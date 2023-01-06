a61afa68d71888124ef9d96286213926223586a4
mariam <mariam.ahhttouche@etu.univ-grenoble-alpes.fr>
2023-01-06 17:57:12 +0100

    replicate

--- scripts/gen_experiments.R
+++ scripts/gen_experiments.R
@@ -2,12 +2,6 @@
 
 library(DoE.wrapper)
 
-lhs_design <- lhs.design( type= "maximin" , nruns= 2000, nfactors= 2 , randomize = TRUE,
-                          digits=0 , seed= 401868 , factor.names=list( size=c(100,1000000),type=c(0,2) ))
-
-
-lhs_design$type[lhs_design$type == 1] <- "Parallel"
-lhs_design$type[lhs_design$type == 0] <- "Sequential"
-lhs_design$type[lhs_design$type == 2] <- "Built-in"
+lhs_design <- lhs.design( type= "maximin" , nruns= 2000, nfactors= 1 , randomize = TRUE, digits=0 , seed= 401868 , factor.names=list( size=c(100,1000000)) )
 
 write.csv(lhs_design,"~/workspace/M2R-ParallelQuicksort/data/experiments/lhs_design.csv", row.names = FALSE)
