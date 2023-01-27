65ba5f937c159f73e1e42ad25544c203dfba3337
mariam <mariam.ahhttouche@etu.univ-grenoble-alpes.fr>
2023-01-27 02:22:52 +0100

    run exp wip

--- scripts/run_experiments.R
+++ scripts/run_experiments.R
@@ -1,10 +1,22 @@
 #!/usr/bin/env Rscript
 
-file_path = "~/workspace/M2R-ParallelQuicksort/data/experiments/lhs_design.csv"
+file_path = "~/workspace/M2R-ParallelQuicksort/data/experiments/experiment.csv"
 
 experiment <- read.csv(file_path)
 
-experiment$type[experiment$type == "Parallel"] <- 1
-experiment$type[experiment$type == "Sequential"] <- 0
-experiment$type[experiment$type == "Built-in"] <- 2
+experiment$Type[experiment$Type == "Parallel"] <- 1
+experiment$Type[experiment$Type == "Sequential"] <- 0
+experiment$Type[experiment$Type == "Built-in"] <- 2
+experiment$Time <- 0
+# print(nrow(experiment))
 
+for(i in 1:nrow(experiment)) {
+    row <- experiment[i,]
+    # print(row$Size)
+    system('echo 'row$Size)
+    x <- '"echo ",row$Size,"\n"'
+    # cat("echo ",row$Size)
+    cat(x)
+    # system('./src/originalParallelQuicksort $row$Size >> row$Time;')
+    # row$Time
+}
